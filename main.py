1df1537f189a1c3e5bd87089cf21d7b7f9dbbca4
lukedxvxes <lukedaviesweb+1@gmail.com>
2023-05-14 15:38:33 +0700

    implement add new items functionality

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import streamlit as st
 import functions
 
+
+def add_todo():
+    new_todo = st.session_state["new_todo"]
+    functions.add_todo(new_todo)
+
+
 todos = functions.read_todos()
-
-
 
 st.title('My Todo App')
 
 for todo in todos:
-    st.checkbox(todo)
+    st.checkbox(todo, key=todo)
+    if st.session_state[todo]:
+        print(todo + ' completed')
 
-st.text_input(label="", placeholder="Enter a todo")
+st.text_input(label="Enter a todo",
+              placeholder="Enter a todo",
+              on_change=add_todo,
+              key="new_todo")
+
+st.session_state
